7a554f021ed24ce122363b7e2adf260604d13b21
Ady-03 <kain.adi0304@gmail.com>
2026-03-30 15:00:45 +0530

    database connection established

---
+++ db.js
@@ -0,0 +1,25 @@
+// responsible for conection between NodeJs and MongoDB
+const mongoose = require("mongoose");
+
+// this is local database URL
+// const mongoURL = 'mongodb://127.0.0.1:27017/myDatabase';
+const mongoURL = process.env.DB_URL; // this is the URL for MongoDB Atlas, stored in .env file for security reasons
+mongoose.connect(mongoURL);
+
+const db = mongoose.connection; //mongoose maintains a default connection
+// object representing the mongodb connection
+// now define event listeners for daabase connection
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+db.on("disconnected", () => {
+  console.log("MongoDB Disconnected");
+});
+
+// export the database connection
+
+module.exports = db;
